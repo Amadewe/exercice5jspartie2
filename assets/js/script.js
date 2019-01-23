ee609f0f16587bfe30f3a789189bd2d03194f725
Amadewe <dewevre.amandine@gmail.com>
2019-01-23 10:23:34 +0100

    Ajout des commentaires pour switch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// L'instruction switch évalue une expression et, selon le résultat obtenu et le cas associé, exécute les instructions correspondantes.
 function changeimage(n) {
 switch (n) {
 case 1:
@@ -21,3 +22,27 @@ document.getElementById("image5").src="assets/img/image5_2.jpg";
 break;
 }
 }
+
+// switch (expression) {
+//   case valeur1:
+//     // Instructions à exécuter lorsque le résultat
+//     // de l'expression correspond à valeur1
+//     instructions1;
+//     [break;]
+//   case valeur2:
+//     // Instructions à exécuter lorsque le résultat
+//     // de l'expression correspond à valeur2
+//     instructions 2;
+//     [break;]
+//   ...
+//   case valeurN:
+//     // Instructions à exécuter lorsque le résultat
+//     // de l'expression à valeurN
+//     instructionsN;
+//     [break;]
+//   [default:
+//     // Instructions à exécuter lorsqu'aucune des valeurs
+//     // ne correspond
+//     instructions_def;
+//     [break;]]
+// }
