77002aad952c1146beed3a082d1371b5689c21f3
Amadewe <dewevre.amandine@gmail.com>
2019-01-28 15:05:39 +0100

    Correction avec plusieurs variantes mais switch fonctionne également

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,29 @@
-// L'instruction switch évalue une expression et, selon le résultat obtenu et le cas associé, exécute les instructions correspondantes.
-// Le switch est semblable à une condition dans laquelle on va pouvoir générer autant d’issues que l’on souhaite.
-function changeimage(n) {
-switch (n) {
-// switch (expression)
-case 1:
-document.getElementById("image1").src='assets/img/image1_2.jpg';
-break;
-
-case 2:
-document.getElementById("image2").src='assets/img/image2_2.jpg';
-break;
-
-case 3:
-document.getElementById("image3").src='assets/img/image3_2.jpg';
-break;
-
-case 4:
-document.getElementById("image4").src='assets/img/image4_2.jpg';
-break;
-
-case 5:
-document.getElementById("image5").src='assets/img/image5_2.jpg';
-break;
+// SOLUTION SANS BONUS
+// solution avec 5 fonctions
+function changeFirstImage() {
+  document.getElementById('image1').src = 'assets/img/image1_2.jpg';
 }
+function changeSecondImage() {
+  document.getElementById('image2').src = 'assets/img/image2_2.jpg';
+}
+function changeThirdImage() {
+  document.getElementById('image3').src = 'assets/img/image3_2.jpg';
+}
+function changeFourthImage() {
+  document.getElementById('image4').src = 'assets/img/image4_2.jpg';
+}
+function changeFifthImage() {
+  document.getElementById('image5').src = 'assets/img/image5_2.jpg';
 }
 
+// SOLUTION DU BONUS
+// fonction permettant de changer les images au survol de chacune d'entre elles
+function switchImages(idImg) {
+  //permet d'aller chercher le nom et l'emplacement des images qui remplacent les images actuelles.
+  document.getElementById(idImg).src = 'assets/img/' + idImg + '_2.jpg';
+}
 
-// tuto: 
-// switch (expression) {
-//   case valeur1:
-//     // Instructions à exécuter lorsque le résultat
-//     // de l'expression correspond à valeur1
-//     instructions1;
-//     [break;]
-//   case valeur2:
-//     // Instructions à exécuter lorsque le résultat
-//     // de l'expression correspond à valeur2
-//     instructions 2;
-//     [break;]
-//   ...
-//   case valeurN:
-//     // Instructions à exécuter lorsque le résultat
-//     // de l'expression à valeurN
-//     instructionsN;
-//     [break;]
-//   [default:
-//     // Instructions à exécuter lorsqu'aucune des valeurs
-//     // ne correspond
-//     instructions_def;
-//     [break;]]
-// }
+//solution par le changement d'images
+function changeImage(img,source){
+  img.src = source;
+}
